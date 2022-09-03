53a25ffe79436067033a1c6bd03299687a334a5f
Muzamil Zaman Khan <95095602+MuzamilDev@users.noreply.github.com>
2022-09-03 17:09:51 +0500

    Add files via upload

---
+++ HowMuchBytesCPlusPlusDataTypesTake.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    cout<<"The Char takes: "<<sizeof(char)<<" bytes"<<endl;
+    cout<<"The Boolean takes: "<<sizeof(bool)<<" bytes"<<endl;
+    cout<<"The Int takes: "<<sizeof(int)<<" bytes"<<endl;
+    cout<<"The Int short takes: "<<sizeof(short)<<" bytes"<<endl;
+    cout<<"The Float takes: "<<sizeof(float)<<" bytes"<<endl;
+    cout<<"The Long takes: " <<sizeof(long)<<" bytes"<<endl;
+    cout<<"The Long Double takes: "<<sizeof(double)<<" bytes"<<endl;
+    return 0;
+}
